1d5cb115729f9cf14dbb9611e32258800952ae25
kouzouman <check@doers-square.com>
2019-01-15 06:20:13 +0900

    フォームの整形

--- components/mail-form.vue
+++ components/mail-form.vue
@@ -1,34 +1,26 @@
 <template>
   <section class="area-block">
     <div class="left-line">
-      <h3 class="headline mb-2">About Me</h3>
+      <h3 class="headline mb-2">Contact</h3>
       <div>
-        <form name="contact" method="POST" data-netlify="true">
+        <form name="contact" method="POST" data-netlify="true" action="/">
           <p>
             <label>Your Name:
-              <input type="text" name="name">
+              <v-text-field name="name"></v-text-field>
             </label>
           </p>
           <p>
             <label>Your Email:
-              <input type="email" name="email">
-            </label>
-          </p>
-          <p>
-            <label>Your Role:
-              <select name="role[]" multiple>
-                <option value="leader">Leader</option>
-                <option value="follower">Follower</option>
-              </select>
+              <v-text-field name="email"></v-text-field>
             </label>
           </p>
           <p>
             <label>Message:
-              <textarea name="message"></textarea>
+              <v-textarea name="message"></v-textarea>
             </label>
           </p>
           <p>
-            <button type="submit">Send</button>
+            <v-btn type="submit">Send</v-btn>
           </p>
         </form>
       </div>
