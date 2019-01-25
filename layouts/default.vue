9c27bf94ba4b5eb0bfdb08663230ccaa89b9e8b4
kouzouman <check@doers-square.com>
2019-01-25 22:57:14 +0900

    formの修正

--- layouts/default.vue
+++ layouts/default.vue
@@ -35,7 +35,7 @@
     </v-footer>
 
     <section class="dummy-form">
-      <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+      <form name="contact" action="/test" netlify netlify-honeypot="bot-field" hidden>
         <input type="hidden" name="form-name" value="contact">
         <input type="text" name="name">
         <input type="email" name="email">
